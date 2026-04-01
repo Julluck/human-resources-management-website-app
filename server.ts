d15a8b86106b61524ca71201fa28ca46706af7ed
unknown <thangjuhi@gmail.com>
2026-04-01 17:41:20 +0700

    fix cors

--- server.ts
+++ server.ts
@@ -331,18 +331,18 @@ async function startServer() {
   });
 
   // Vite middleware for development
-  if (process.env.NODE_ENV !== "production") {
-    const vite = await createViteServer({
-      server: { middlewareMode: true },
-      appType: "spa",
-    });
-    app.use(vite.middlewares);
-  } else {
-    app.use(express.static(path.join(__dirname, "dist")));
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "dist", "index.html"));
-    });
-  }
+  //if (process.env.NODE_ENV !== "production") {
+    //const vite = await createViteServer({
+      //server: { middlewareMode: true },
+      //appType: "spa",
+    //});
+    //app.use(vite.middlewares);
+  //} else {
+    //app.use(express.static(path.join(__dirname, "dist")));
+    //app.get("*", (req, res) => {
+      //res.sendFile(path.join(__dirname, "dist", "index.html"));
+    //});
+  //}
 
   const PORT = 3000;
   app.listen(PORT, "0.0.0.0", () => {
